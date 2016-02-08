f2b738ce3d655510ba4b8c4be355781fb1fb5ff4
fargs <lesliefarago@hotmail.com>
2016-02-07 19:41:10 -0500

    Add multiple days at a time. Add day refactored into its own view.

--- WebApp/Controllers/AvailabilityController.cs
+++ WebApp/Controllers/AvailabilityController.cs
@@ -10,9 +10,11 @@ using Model.Enums;
 using WebApp.ViewModels.AvailabilityViewModels;
 using System.Globalization;
 using WebApp.Library;
+using System.Net;
 
 namespace WebApp.Controllers
 {
+    [Authorize]
     public class AvailabilityController : Controller
     {
         OrvosiEntities db = new OrvosiEntities();
@@ -44,34 +46,69 @@ namespace WebApp.Controllers
                 CurrentUser = user,
                 SelectedUser = selectedUser,
                 Calendar = CultureInfo.CurrentCulture.Calendar,
-                Today = SystemTime.Now(),
-                NewAvailableDay = new AvailableDay()
-                {
-                    PhysicianId = selectedUser.Id,
-                    Day = SystemTime.Now()
-                }
+                Today = SystemTime.Now()
             };
-
-            var arr = model.AvailableDays.Select(c => string.Format("'{0}'", c.Day.ToString("yyyy-MM-dd"))).ToArray<string>();
-            ViewBag.AvailableDaysCSV = MvcHtmlString.Create(string.Join(",", arr));
 
             return View(model);
         }
 
-        [HttpPost]
-        public async Task<ActionResult> AddDay(AvailableDay newDay)
+        [HttpGet]
+        public async Task<ActionResult> AddDay(string id)
         {
+            if (string.IsNullOrEmpty(id))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var physician = db.Users.Single(c => c.Id == id);
+
+            var availableDays = await db.AvailableDays.Where(c => c.PhysicianId == id).ToListAsync();
+
+            var arr = availableDays.Select(c => string.Format("'{0}'", c.Day.ToString("yyyy-MM-dd"))).ToArray<string>();
+            ViewBag.AvailableDaysCSV = MvcHtmlString.Create(string.Join(",", arr));
+
+            var model = new AvailableDay()
+            {
+                PhysicianId = id,
+                PhysicianName = physician.DisplayName,
+                Day = SystemTime.Now()
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddDay(AvailableDay model)
+        {
+            var user = db.Users.Single(u => u.UserName == User.Identity.Name);
+
+            // Admins will be able to manage available days on behalf of others, other roles can only manage their own.
+            if (user.RoleCategoryId != RoleCategory.Admin && model.PhysicianId != user.Id)
+            {
+                throw new Exception("You are not allowed to update this users information.");
+            }
+
             if (ModelState.IsValid)
             {
                 using (var db = new OrvosiEntities(User.Identity.Name))
                 {
-                    db.AvailableDays.Add(newDay);
+                    var dates = Request.Form["AvailableDays"].Split(',');
+                    foreach (var item in dates)
+                    {
+                        var day = new AvailableDay()
+                        {
+                            Day = DateTime.Parse(item),
+                            PhysicianId = model.PhysicianId,
+                            CompanyId = model.CompanyId,
+                            LocationId = model.LocationId,
+                            IsPrebook = model.IsPrebook
+                        };
+                        db.AvailableDays.Add(day);
+                    }
                     await db.SaveChangesAsync();
-                    //await db.Entry(obj).ReloadAsync();
-                    return RedirectToAction("Index", new { id = newDay.PhysicianId });
                 }
+                return RedirectToAction("Index", new { id = model.PhysicianId });
             }
-            return Redirect(Request.UrlReferrer.ToString());
+            return View(model);
         }
     }
 }

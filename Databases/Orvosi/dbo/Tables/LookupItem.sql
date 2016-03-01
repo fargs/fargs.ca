840b50e26fa0d1dba21d2411cdf7f0cdd8df3ed7
fargs <lesliefarago@hotmail.com>
2016-02-29 23:43:24 -0500

    Added locations to the LookupItem table. Added  a ShortText column so we can have alternate short text for lookups.

--- Databases/Orvosi/dbo/Tables/LookupItem.sql
+++ Databases/Orvosi/dbo/Tables/LookupItem.sql
@@ -5,8 +5,11 @@
     [Value]        SMALLINT       NULL,
     [ModifiedDate] DATETIME       CONSTRAINT [DF_LookupItem_ModifiedDate] DEFAULT (getdate()) NOT NULL,
     [ModifiedUser] NVARCHAR (100) CONSTRAINT [DF_LookupItem_ModifiedUser] DEFAULT (suser_name()) NOT NULL,
+    [ShortText]    NVARCHAR (50)  NULL,
     CONSTRAINT [PK_LookupItem] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
 
 

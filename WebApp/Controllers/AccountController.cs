b0c020713febcf412cedb969f56d3c2e35f03c2c
mirhagk <mirhagk@gmail.com>
2016-03-18 09:08:33 -0400

    Removed confusing incorrect comment

--- WebApp/Controllers/AccountController.cs
+++ WebApp/Controllers/AccountController.cs
@@ -108,9 +108,7 @@ namespace WebApp.Controllers
                 ViewBag.errorMessage = "Your account is pending activation by our administrators. You should be receiving an activation email shortly.";
                 return View("Error");
             }
-
-            // This doesn't count login failures towards account lockout
-            // To enable password failures to trigger account lockout, change to shouldLockout: true
+            
             var result = await SignInManager.PasswordSignInAsync(user.UserName, model.Password, model.RememberMe, shouldLockout: true);
             switch (result)
             {

e4eb4495200aabc9c7c6be64a8031ae68bc3e0f4
fargs <lesliefarago@hotmail.com>
2016-04-04 00:57:56 -0400

    Fixed bug - Tasks  with no dependencies were not showing up in the Dashboard task list.

--- Databases/Orvosi/API/Stored Procedures/GetMyTasks.sql
+++ Databases/Orvosi/API/Stored Procedures/GetMyTasks.sql
@@ -1,5 +1,4 @@
 ﻿
-
 CREATE PROC [API].[GetMyTasks]
 	@AssignedTo UNIQUEIDENTIFIER
 	, @Now DATETIME
@@ -29,10 +28,10 @@ AS (
 			, IsDependentOnExamDate
 			, DueDateBase
 			, DueDateDiff
-			, CAST('<XMLRoot><RowData>' + REPLACE(DependsOn,',','</RowData><RowData>') + '</RowData></XMLRoot>' AS XML) AS x
+			, CAST('<XMLRoot><RowData>' + REPLACE(CASE WHEN DependsOn IS NULL THEN '' ELSE DependsOn END,',','</RowData><RowData>') + '</RowData></XMLRoot>' AS XML) AS x
 		FROM   dbo.ServiceRequestTask
 		WHERE IsObsolete = 0 AND CompletedDate IS NULL -- Because this is a stored proc, I want to control the amount of records returned here, not in the service layer.
-			AND AssignedTo = @AssignedTo
+			AND AssignedTo = '8dd4e180-6e3a-4968-a00d-eeb6d2cc7f0c'
 	)t
 	CROSS APPLY x.nodes('/XMLRoot/RowData')m(n)
 ) 

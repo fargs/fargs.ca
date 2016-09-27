0efb1e4c6f1962fc04b1da8510586987abd0f71a
fargs <lesliefarago@gmail.com>
2016-09-26 23:44:56 -0400

    Added the API AvailableSlot controller as it is being used by the Reschedule page.

---
+++ WebApp/API/AvailableSlotController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Orvosi.Data;
+using System.Data.Entity;
+using System.Web.Http.Results;
+
+namespace WebApp.API
+{
+    public class AvailableSlotController : ApiController
+    {
+        OrvosiDbContext db = new OrvosiDbContext();
+
+        [Route("api/physician/{physicianId}/day/{day}/slots")]
+        public HttpResponseMessage GetByAvailableDay(DateTime day, Guid physicianId)
+        {
+            var ad = db.AvailableDays
+                .SingleOrDefault(c => c.PhysicianId == physicianId && c.Day == day);
+
+            if (ad == null)
+            {
+                return this.Request.CreateResponse(HttpStatusCode.NotFound, "Not available this day.");
+            }
+
+            return this.Request.CreateResponse(
+                HttpStatusCode.OK,
+                new
+                {
+                    Day = ad.Day,
+                    IsPrebook = ad.IsPrebook,
+                    CompanyId = ad.CompanyId,
+                    CompanyName = ad.Company.Name,
+                    LocationId = ad.LocationId,
+                    LocationName = ad.Address.Name,
+                    LocationOwner = ad.Address.OwnerGuid,
+                    Slots = ad.AvailableSlots
+                        .OrderBy(s => s.StartTime)
+                        .Select(s => new
+                        {
+                            Id = s.Id,
+                            StartTime = s.StartTime,
+                            Duration = s.Duration,
+                            Title = s.Id,
+                            IsAvailable = !s.ServiceRequests.Any()
+                        })
+                });
+        }
+    }
+}

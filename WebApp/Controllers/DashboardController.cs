f2fe4d3e63cf543f51c26f209daed0c4a6912528
fargs <lesliefarago@gmail.com>
2016-10-10 23:27:11 -0400

    Removed unused code.

--- WebApp/Controllers/DashboardController.cs
+++ WebApp/Controllers/DashboardController.cs
@@ -19,46 +19,9 @@ namespace WebApp.Controllers
     {
         private Orvosi.Data.OrvosiDbContext context = new Orvosi.Data.OrvosiDbContext();
 
-        public async Task<ActionResult> Index(Guid? serviceProviderId, bool showClosed = false, bool onlyMine = true)
+        public ActionResult Index(Guid? serviceProviderId, bool showClosed = false, bool onlyMine = true)
         {
             return RedirectToAction("Today");
-            //// Set date range variables used in where conditions
-            //var now = SystemTime.Now();
-            //var loggedInUserId = User.Identity.GetGuidUserId();
-            //var baseUrl = Request.GetBaseUrl();
-
-            //Guid? userId = User.Identity.GetGuidUserId();
-            //// Admins can see the Service Provider dropdown and view other's dashboards. Otherwise, it displays the data of the current user.
-            //if (User.Identity.IsAdmin() && serviceProviderId.HasValue)
-            //{
-            //    userId = serviceProviderId.Value;
-            //}
-
-            //var requests = await context.GetAssignedServiceRequestsAsync(userId, now, showClosed, null);
-
-            //// Populate the view model
-            //var vm = new dvm.IndexViewModel();
-
-            //vm.WeekFolders = ServiceRequestMapper.MapToWeekFolders(requests, now, loggedInUserId, baseUrl);
-            //vm.AddOns = ServiceRequestMapper.MapToAddOns(requests, now, loggedInUserId, baseUrl);
-            //vm.Today = ServiceRequestMapper.MapToToday(requests, now, loggedInUserId, baseUrl);
-            //vm.DueDates = ServiceRequestMapper.MapToDueDates(requests, now, loggedInUserId, baseUrl);
-
-            //// Additional view data.
-            //vm.SelectedUserId = userId;
-            //vm.UserSelectList = (from user in context.AspNetUsers
-            //                    from userRole in context.AspNetUserRoles
-            //                    from role in context.AspNetRoles
-            //                    where user.Id == userRole.UserId && role.Id == userRole.RoleId
-            //                    select new SelectListItem
-            //                    {
-            //                        Text = user.FirstName + " " + user.LastName,
-            //                        Value = user.Id.ToString(),
-            //                        Group = new SelectListGroup() { Name = role.Name }
-            //                    }).ToList();
-            
-            //return new NegotiatedResult("Index", vm);
-            
         }
 
         public async Task<ActionResult> Today(Guid? serviceProviderId)

b7d07c37d9ac12a3e75a05364c66594bbe18a0ad
fargs <lesliefarago@gmail.com>
2016-11-17 15:24:42 -0500

    Added 15 minutes to the durations on the availability page.

--- WebApp/API/AvailableSlotController.cs
+++ WebApp/API/AvailableSlotController.cs
@@ -43,7 +43,7 @@ namespace WebApp.API
                             Id = s.Id,
                             StartTime = s.StartTime,
                             Duration = s.Duration,
-                            Title = s.Id,
+                            Title = (s.ServiceRequests.Any() ? s.ServiceRequests.First().ClaimantName + " - " + s.ServiceRequests.First().Id.ToString() : string.Empty),
                             IsAvailable = !s.ServiceRequests.Any()
                         })
                 });

3155d6a0b20f068afb799107a3232ac153d6a3b3
fargs <lesliefarago@gmail.com>
2016-11-28 08:58:28 -0500

    Added some Data initialization scripts

---
+++ Databases/Orvosi/_Data/ReadMe.md
@@ -0,0 +1,14 @@
+﻿# Initializing the Database
+
+Execute the files in this folder (_Data) in the following order:
+
+1. AddressType
+2. RoleCategory
+2. AspNetRoles
+3. TimeZone
+5. Country
+4. Province
+5. City
+6. ServiceCategory
+8. TaskStatus
+10. PhysicianSpeciality
---
+++ Databases/Orvosi/_Data/dbo.PhysicianSpeciality.data.sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[PhysicianSpeciality] ([Id], [Name], [Description], [ModifiedDate], [ModifiedUser]) VALUES (27, N'Physiatry', NULL, N'2016-07-08 13:48:22', N'MicrosoftAccount\lesliefarago@hotmail.com')
+INSERT INTO [dbo].[PhysicianSpeciality] ([Id], [Name], [Description], [ModifiedDate], [ModifiedUser]) VALUES (28, N'Orthopedics', NULL, N'2016-07-08 13:48:22', N'MicrosoftAccount\lesliefarago@hotmail.com')
+INSERT INTO [dbo].[PhysicianSpeciality] ([Id], [Name], [Description], [ModifiedDate], [ModifiedUser]) VALUES (29, N'Neurology', NULL, N'2016-07-08 13:48:22', N'MicrosoftAccount\lesliefarago@hotmail.com')

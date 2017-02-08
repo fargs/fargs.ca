1300c03de90dc5c93480fadd73c2617473f6c1bb
fargs <lesliefarago@gmail.com>
2017-02-07 22:30:35 -0500

    Hangfire Initial release. (#262)
    Added 1 recurring job to Update Assessment Day Task State.

---
+++ Orvosi.Hangfire/AuthorizeRoleFilter.cs
@@ -0,0 +1,18 @@
+﻿using Hangfire.Dashboard;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire.Annotations;
+
+namespace Orvosi.Hangfire
+{
+    public class AuthorizeRoleFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize([NotNull] DashboardContext context)
+        {
+            return true;
+        }
+    }
+}
---
+++ Orvosi.Hangfire/WorkJobs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Orvosi.Hangfire
+{
+    public class WorkJobs
+    {
+        string connectionString;
+        public WorkJobs(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public void UpdateAssessmentDayState()
+        {
+            using (IDbConnection dbConnection = new SqlConnection(connectionString))
+            {
+                dbConnection.Open();
+                dbConnection.Execute("API.UpdateAssessmentDayTaskState @Now", new { Now = DateTime.UtcNow });
+            }
+        }
+    }
+}

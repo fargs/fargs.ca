da195edf85359acb62c5f5fb23534bab3d64d5ef
fargs <lesliefarago@gmail.com>
2017-04-24 12:49:16 -0400

    Missing static data file for db initialization.

---
+++ Databases/Orvosi/_Data/dbo.PhysicianCompanyStatus.data.sql
@@ -0,0 +1,6 @@
+INSERT INTO [dbo].[PhysicianCompanyStatus] ([Id], [Name]) VALUES (1, N'Interested')
+INSERT INTO [dbo].[PhysicianCompanyStatus] ([Id], [Name]) VALUES (2, N'Not interested')
+INSERT INTO [dbo].[PhysicianCompanyStatus] ([Id], [Name]) VALUES (3, N'In progress')
+INSERT INTO [dbo].[PhysicianCompanyStatus] ([Id], [Name]) VALUES (4, N'Active')
+INSERT INTO [dbo].[PhysicianCompanyStatus] ([Id], [Name]) VALUES (5, N'Assessor Package Submitted')
+INSERT INTO [dbo].[PhysicianCompanyStatus] ([Id], [Name]) VALUES (6, N'Ready to Work')

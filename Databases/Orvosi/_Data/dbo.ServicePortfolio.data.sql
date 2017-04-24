17daef956ad7e8917a4079d22edd4d53a4b61f5b
fargs <lesliefarago@gmail.com>
2017-04-24 13:07:03 -0400

    added missing ServicePortfolio data file.

---
+++ Databases/Orvosi/_Data/dbo.ServicePortfolio.data.sql
@@ -0,0 +1,4 @@
+SET IDENTITY_INSERT [dbo].[ServicePortfolio] ON
+INSERT INTO [dbo].[ServicePortfolio] ([Id], [Name], [Description], [ModifiedDate], [ModifiedUser]) VALUES (1, N'Orvosi', NULL, N'2015-10-23 16:18:51', N'orvosi-sqladmin')
+INSERT INTO [dbo].[ServicePortfolio] ([Id], [Name], [Description], [ModifiedDate], [ModifiedUser]) VALUES (2, N'Physician', NULL, N'2015-10-23 16:18:51', N'orvosi-sqladmin')
+SET IDENTITY_INSERT [dbo].[ServicePortfolio] OFF

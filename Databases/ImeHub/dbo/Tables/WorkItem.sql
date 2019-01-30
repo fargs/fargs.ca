4bbf5ff4b20d70c184ea7e6b4411317e6793ada6
fargs <lesliefarago@gmail.com>
2019-01-30 15:58:18 -0500

    missed database changes

--- Databases/ImeHub/dbo/Tables/WorkItem.sql
+++ Databases/ImeHub/dbo/Tables/WorkItem.sql
@@ -4,18 +4,21 @@
     [SysEndTime]                        DATETIME2 (7) GENERATED ALWAYS AS ROW END   NOT NULL,
     [Sequence]                          SMALLINT                                    NOT NULL,
     [Name]                              NVARCHAR (128)                              NOT NULL,
-    [RoleId]                            UNIQUEIDENTIFIER                            NULL,
     [IsBaselineDate]                    BIT                                         CONSTRAINT [DF_WorkItem_IsBaselineDate] DEFAULT ((0)) NOT NULL,
     [DueDateDurationFromBaseline]       SMALLINT                                    NULL,
     [EffectiveDateDurationFromBaseline] SMALLINT                                    NULL,
     [IsCriticalPath]                    BIT                                         CONSTRAINT [DF_WorkItem_IsCriticalPath] DEFAULT ((0)) NOT NULL,
     [IsBillable]                        BIT                                         CONSTRAINT [DF_WorkItem_IsBillable] DEFAULT ((0)) NOT NULL,
     [WorkflowId]                        UNIQUEIDENTIFIER                            NOT NULL,
+    [TeamRoleId]                        UNIQUEIDENTIFIER                            NOT NULL,
     CONSTRAINT [PK_WorkItem] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_WorkItem_TeamRole] FOREIGN KEY ([TeamRoleId]) REFERENCES [dbo].[TeamRole] ([Id]),
     CONSTRAINT [FK_WorkItem_Workflow] FOREIGN KEY ([WorkflowId]) REFERENCES [dbo].[Workflow] ([Id]) ON DELETE CASCADE ON UPDATE CASCADE,
     PERIOD FOR SYSTEM_TIME ([SysStartTime], [SysEndTime])
 )
 WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE=[dbo].[WorkItemHistory], DATA_CONSISTENCY_CHECK=ON));
+
+
 
 
 
--- Databases/ImeHub/dbo/Tables/WorkItemHistory.sql
+++ Databases/ImeHub/dbo/Tables/WorkItemHistory.sql
@@ -4,14 +4,16 @@
     [SysEndTime]                        DATETIME2 (7)    NOT NULL,
     [Sequence]                          SMALLINT         NOT NULL,
     [Name]                              NVARCHAR (128)   NOT NULL,
-    [RoleId]                            UNIQUEIDENTIFIER NULL,
     [IsBaselineDate]                    BIT              NOT NULL,
     [DueDateDurationFromBaseline]       SMALLINT         NULL,
     [EffectiveDateDurationFromBaseline] SMALLINT         NULL,
     [IsCriticalPath]                    BIT              NOT NULL,
     [IsBillable]                        BIT              NOT NULL,
-    [WorkflowId]                        UNIQUEIDENTIFIER NOT NULL
+    [WorkflowId]                        UNIQUEIDENTIFIER NOT NULL,
+    [TeamRoleId]                        UNIQUEIDENTIFIER NOT NULL
 );
+
+
 
 
 
